6c6253b55b849757e43dbbb9d050e4f829a3e525
slaystarthunder <72455891+slaystarthunder@users.noreply.github.com>
2023-11-22 13:44:12 +0100

    Added game logic
    Added logic for when a player wins the game.

--- src/GUI.java
+++ src/GUI.java
@@ -6,159 +6,164 @@ import java.util.Arrays;
 
 public class GUI extends JFrame implements ActionListener {
 
-        private JButton[] buttons = new JButton[9];
-        private CardLayout cardLayout = new CardLayout();
-        private JPanel cardPanel = new JPanel(cardLayout);
+    private JButton[] buttons = new JButton[9];
+    private CardLayout cardLayout = new CardLayout();
+    private JPanel cardPanel = new JPanel(cardLayout);
 
-        public GUI() {
-            super("Tic Tac Toe");
-            setSize(1080, 640);
-            setResizable(false);
-            setDefaultCloseOperation(EXIT_ON_CLOSE);
+    public GUI() {
+        super("Tic Tac Toe");
+        setSize(1080, 640);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
 
-            add(cardPanel);
+        add(cardPanel);
 
-            JPanel gamePanel = gamePanel();
+        JPanel gamePanel = gamePanel();
 
-            JPanel menuPanel = menuPanel();
+        JPanel menuPanel = menuPanel();
 
-            JPanel creditPanel = creditPanel();
+        JPanel creditPanel = creditPanel();
 
-            cardPanel.add(menuPanel, "Menu");
-            cardPanel.add(gamePanel, "Game");
-            cardPanel.add(creditPanel,"Credit");
-            //Visar menyn när programmet startar
-            cardLayout.show(cardPanel, "Menu");
+        cardPanel.add(menuPanel, "Menu");
+        cardPanel.add(gamePanel, "Game");
+        cardPanel.add(creditPanel,"Credit");
+        //Visar menyn när programmet startar
+        cardLayout.show(cardPanel, "Menu");
 
-            setVisible(true);
-        }
-        //Menuknappen
-        public JButton mainMenuButton(){
-            JButton mainMenuButton = new JButton("Main Menu");
-            mainMenuButton.setPreferredSize(new Dimension(200,50));
-            mainMenuButton.addActionListener(e -> cardLayout.show(cardPanel, "Menu"));
-            mainMenuButton.setBackground(new Color(242, 204, 195));
-            mainMenuButton.setForeground(new Color(115, 108, 237));
-            mainMenuButton.setFont(new Font("Serif",Font.BOLD,30));
-            return mainMenuButton;
-        }
-        //Spelpanelen som skapar gridlayout till spelet och en panel för information
-        private JPanel gamePanel() {
+        setVisible(true);
+    }
+    //Menuknappen
+    public JButton mainMenuButton(){
+        JButton mainMenuButton = new JButton("Main Menu");
+        mainMenuButton.setPreferredSize(new Dimension(200,50));
+        mainMenuButton.addActionListener(e -> cardLayout.show(cardPanel, "Menu"));
+        mainMenuButton.setBackground(new Color(242, 204, 195));
+        mainMenuButton.setFont(new Font("Serif",Font.BOLD,30));
+        return mainMenuButton;
+    }
+    //Spelpanelen som skapar gridlayout till spelet och en panel för information
+    private JPanel gamePanel() {
 
-            JPanel mainPanel = new JPanel(new BorderLayout());
+        JPanel mainPanel = new JPanel(new BorderLayout());
 
-            JPanel gameBoard = new JPanel(new GridLayout(3, 3, 10, 10));
-            gameBoard.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
-            gameBoard.setBackground(new Color(242, 223, 215));
-            playButtons(gameBoard);
-            mainPanel.add(gameBoard, BorderLayout.CENTER);
+        JPanel gameBoard = new JPanel(new GridLayout(3, 3, 10, 10));
+        gameBoard.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+        playButtons(gameBoard);
+        mainPanel.add(gameBoard, BorderLayout.CENTER);
 
-            JLabel scoreName = new JLabel("Scoreboard:", SwingConstants.LEFT);
-            scoreName.setFont(new Font("Serif", Font.BOLD, 20));
+        JLabel scoreName = new JLabel("Scoreboard:", SwingConstants.LEFT);
+        scoreName.setFont(new Font("Serif", Font.BOLD, 20));
 
-            JLabel whosTurn = new JLabel("Your turn: ", SwingConstants.RIGHT);
-            whosTurn.setFont(new Font("Serif", Font.BOLD, 20));
+        JLabel whosTurn = new JLabel("Your turn: ", SwingConstants.RIGHT);
+        whosTurn.setFont(new Font("Serif", Font.BOLD, 20));
 
-            JPanel scorePanel = new JPanel(new BorderLayout());
-            scorePanel.add(scoreName, BorderLayout.WEST);
-            scorePanel.add(whosTurn, BorderLayout.EAST);
-            scorePanel.setPreferredSize(new Dimension(200, 100));
-            scorePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
-            scorePanel.setBackground(Color.WHITE);
-            mainPanel.add(scorePanel, BorderLayout.NORTH);
-            mainPanel.add(mainMenuButton(),BorderLayout.SOUTH);
+        JPanel scorePanel = new JPanel(new BorderLayout());
+        scorePanel.add(scoreName, BorderLayout.WEST);
+        scorePanel.add(whosTurn, BorderLayout.EAST);
+        scorePanel.setPreferredSize(new Dimension(200, 100));
+        scorePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+        scorePanel.setBackground(Color.WHITE);
+        mainPanel.add(scorePanel, BorderLayout.NORTH);
+        mainPanel.add(mainMenuButton(),BorderLayout.SOUTH);
 
-            return mainPanel;
-        }
-        //MainMenu sidan som visar de olika alternativen man har i menyn.
-        private JPanel menuPanel() {
-            JPanel menuPanel = new JPanel();
-            menuPanel.setLayout(new GridLayout(4, 2, 10, 10));
+        return mainPanel;
+    }
+    //MainMenu sidan som visar de olika alternativen man har i menyn.
+    private JPanel menuPanel() {
+        JPanel menuPanel = new JPanel();
+        menuPanel.setLayout(new GridLayout(4, 2, 10, 10));
 
-            JLabel mainMenuLabel = new JLabel("Tic Tac Toe", SwingConstants.CENTER);
-            mainMenuLabel.setFont(new Font("Serif",Font.BOLD,100));
-            mainMenuLabel.setForeground(new Color(115, 108, 237));
-            menuPanel.add(mainMenuLabel,BorderLayout.NORTH);
+        JLabel mainMenuLabel = new JLabel("Tic Tac Toe", SwingConstants.CENTER);
+        mainMenuLabel.setFont(new Font("Serif",Font.BOLD,100));
+        mainMenuLabel.setForeground(new Color(115, 108, 237));
+        menuPanel.add(mainMenuLabel,BorderLayout.NORTH);
 
-            JButton startButton = mainMenuButton("Start Game");
-            JButton creditButton = mainMenuButton("Credit");
-            JButton exitButton = mainMenuButton("Exit Game");
+        JButton startButton = mainMenuButton("Start Game");
+        JButton creditButton = mainMenuButton("Credit");
+        JButton exitButton = mainMenuButton("Exit Game");
 
-            startButton.addActionListener(e -> cardLayout.show(cardPanel, "Game"));
-            exitButton.addActionListener(e -> System.exit(0));
-            creditButton.addActionListener(e -> cardLayout.show(cardPanel,"Credit"));
-            //Lägger till knapparna
-            menuPanel.add(startButton);
-            menuPanel.add(creditButton);
-            menuPanel.add(exitButton);
+        startButton.addActionListener(e -> cardLayout.show(cardPanel, "Game"));
+        exitButton.addActionListener(e -> System.exit(0));
+        creditButton.addActionListener(e -> cardLayout.show(cardPanel,"Credit"));
+        //Lägger till knapparna
+        menuPanel.add(startButton);
+        menuPanel.add(creditButton);
+        menuPanel.add(exitButton);
 
-            menuPanel.setBackground(new Color(242, 223, 215));
+        menuPanel.setBackground(new Color(242, 223, 215));
 
-            return menuPanel;
-        }
-        private JButton mainMenuButton(String text) {
-            JButton button = new JButton(text);
-            button.setFont(new Font("Times New Roman", Font.BOLD, 30));
-            button.setBackground(new Color(212, 193, 236));
-            button.setForeground(new Color(115, 108, 237));
-            button.setAlignmentX(Component.CENTER_ALIGNMENT);
-            button.setMaximumSize(new Dimension(200, 50));
-            return button;
-        }
-        //Credit sidan som visar vilka som har gjort programmet
-        private JPanel creditPanel(){
-            JPanel creditPanel = new JPanel(new BorderLayout());
+        return menuPanel;
+    }
+    private JButton mainMenuButton(String text) {
+        JButton button = new JButton(text);
+        button.setFont(new Font("Times New Roman", Font.BOLD, 30));
+        button.setBackground(new Color(212, 193, 236));
+        button.setForeground(Color.BLACK);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setMaximumSize(new Dimension(200, 50));
+        return button;
+    }
+    //Credit sidan som visar vilka som har gjort programmet
+    private JPanel creditPanel(){
+        JPanel creditPanel = new JPanel(new BorderLayout());
 
-            //Headern
-            JLabel creditHeader = new JLabel("Credits", SwingConstants.CENTER);
-            creditHeader.setFont(new Font("Serif", Font.BOLD, 100));
-            creditHeader.setForeground(new Color(115, 108, 237));
-            creditPanel.add(creditHeader,BorderLayout.NORTH);
+        //Headern
+        JLabel creditHeader = new JLabel("Credits", SwingConstants.CENTER);
+        creditHeader.setFont(new Font("Serif", Font.BOLD, 100));
+        creditPanel.add(creditHeader,BorderLayout.NORTH);
 
-            // Information om vem som har gjort programmet
-            JTextArea creditTextField = new JTextArea(" Adam Barnell\n Oliver Schuller\n Carl Sundberg\n Reidar Thorsen");
-            creditTextField.setEditable(false);
-            creditTextField.setLineWrap(true);
-            creditTextField.setFont(new Font("Serif", Font.BOLD, 50));
-            creditTextField.setBackground(new Color(242, 223, 215));
-            creditTextField.setForeground(new Color(115, 108, 237));
-            creditPanel.add(creditTextField, BorderLayout.CENTER);
+        // Information om vem som har gjort programmet
+        JTextArea creditTextField = new JTextArea(" Adam Barnell\n Oliver Schuller\n Carl Sundberg\n Reidar Thorsen");
+        creditTextField.setEditable(false);
+        creditTextField.setLineWrap(true);
+        creditTextField.setFont(new Font("Serif", Font.BOLD, 50));
+        creditTextField.setBackground(new Color(242, 223, 215));
+        creditTextField.setForeground(new Color(115, 108, 237));
+        creditPanel.add(creditTextField, BorderLayout.CENTER);
 
-            creditPanel.add(mainMenuButton(), BorderLayout.SOUTH);
+        creditPanel.add(mainMenuButton(), BorderLayout.SOUTH);
 
-            return creditPanel;
-        }
+        return creditPanel;
+    }
 
-        private void playButtons(JPanel gameBoard) {
-            for (int i = 0; i < 9; i++) {
-                buttons[i] = new JButton(String.valueOf(i+1));
-                buttons[i].addActionListener(this);
-                buttons[i].setFont(new Font("Serif", Font.BOLD,40));
-                buttons[i].setBackground(Color.lightGray);
-                buttons[i].setBorder(BorderFactory.createLineBorder(Color.black, 2));
-                gameBoard.add(buttons[i]);
-            }
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            // Endast test atm, bör göras som en funktion sen
-            try {
-                // Kollar vilken spelare som startar och lägger sen in X/O på vald plats
-                if (Player.getPlayerTurn()) {
-                    Player.setBoard(Integer.parseInt(e.getActionCommand())-1);
-                    buttons[Integer.parseInt(e.getActionCommand())-1].setText("X");
-                    Player.setPlayerTurn(false);
-                } else {
-                    Player.setBoard(Integer.parseInt(e.getActionCommand())-1);
-                    buttons[Integer.parseInt(e.getActionCommand())-1].setText("O");
-                    Player.setPlayerTurn(true);
-                }
-            }
-            // Ifall en redan tagen plats väljs så skapar Java ett error, hantera det och skicka felmeddelande till användare
-            catch (Exception ex) {
-                JOptionPane.showMessageDialog(null, "Invalid choice, try again");
-            }
+    private void playButtons(JPanel gameBoard) {
+        for (int i = 0; i < 9; i++) {
+            buttons[i] = new JButton(String.valueOf(i+1));
+            buttons[i].addActionListener(this);
+            buttons[i].setFont(new Font("Serif", Font.BOLD,40));
+            buttons[i].setBackground(Color.lightGray);
+            buttons[i].setBorder(BorderFactory.createLineBorder(Color.black, 2));
+            gameBoard.add(buttons[i]);
         }
     }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Endast test atm, bör göras som en funktion sen
+        try {
+            // Kollar vilken spelare som startar och lägger sen in X/O på vald plats
+            if (Player.getPlayerTurn()) {
+                Player.setBoard(Integer.parseInt(e.getActionCommand())-1);
+                buttons[Integer.parseInt(e.getActionCommand())-1].setText("X");
+                Player.setPlayerTurn(false);
+            } else {
+                Player.setBoard(Integer.parseInt(e.getActionCommand())-1);
+                buttons[Integer.parseInt(e.getActionCommand())-1].setText("O");
+                Player.setPlayerTurn(true);
+            }
+        }
+        // Ifall en redan tagen plats väljs så skapar Java ett error, hantera det och skicka felmeddelande till användare
+        catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Invalid choice, try again");
+        }
+
+        String winner = Player.checkForWin();
+        if (!winner.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Game over!\n " + winner + " wins!");
+        } else if (Player.isBoardFull()) {
+            JOptionPane.showMessageDialog(null, "It's a draw!");
+        }
+    }
+}
--- src/Player.java
+++ src/Player.java
@@ -4,8 +4,21 @@ public class Player {
     private static boolean playerTurn;
     private static String[] gameBoard = new String[9];
 
+    private static final int[][] winningCombinations = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3 ,6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6},
+    };
+
     Player() {
+
         randomPlayer();
+        Arrays.fill(gameBoard, "");
     }
 
     // Randomiserar huruvida playerOne är True eller False
@@ -31,6 +44,30 @@ public class Player {
             gameBoard[position] = "O";
         }
     }
+
+    public static boolean isBoardFull() {
+        for (String cell : gameBoard) {
+            if (cell.isEmpty()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static String checkForWin() {
+        for (int[] combination : winningCombinations) {
+
+            String cell1 = gameBoard[combination[0]];
+            String cell2 = gameBoard[combination[1]];
+            String cell3 = gameBoard[combination[2]];
+
+            if (cell1 != null && cell1.equals(cell2) && cell1.equals(cell3) && !cell1.isEmpty()) {
+                return cell1;
+            }
+        }
+        return "";
+    }
+
 
     public static String[] getGameBoard(){
         return gameBoard;
